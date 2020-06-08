c3b5c389722bd14eda75f7ef1e29184354539aca
Evan Haggard <evanhaggard123@gmail.com>
2020-06-08 13:12:01 -0400

    first commit

---
+++ src/main/java/hi/haggardinnovations/postgresdemo/repository/AnswerRepository.java
@@ -0,0 +1,11 @@
+package hi.haggardinnovations.postgresdemo.repository;
+
+import hi.haggardinnovations.postgresdemo.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionId(Long questionId);
+}
